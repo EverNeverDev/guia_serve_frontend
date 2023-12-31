74eaf51e2211cb2cca315cd98ec09489cad68a21
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-30 23:41:54 -0300

    :wrench: installed firebase

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    firebase: {
+      apiKey: "AIzaSyAly1yie_3UW8qL29CeVI78rT1y8WlHArI",
+      authDomain: "guia-serve-73d14.firebaseapp.com",
+      projectId: "guia-serve-73d14",
+      storageBucket: "guia-serve-73d14.appspot.com",
+      messagingSenderId: "663948503664",
+      appId: "1:663948503664:web:ba42e02b5345e1ccc4e2b5",
+      measurementId: "G-P1DREBDCP8",
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
