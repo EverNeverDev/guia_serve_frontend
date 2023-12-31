404f01ded2060b32d57c89901feb702edcbdc63c
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-31 00:03:50 -0300

    :recycle: added index.ts in types folder

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+import IAddress from "./IAddress";
+import IUser from "./IUser";
+import IProvider from "./IProvider";
+import ICategory from "./ICategory";
+import IChat from "./IChat";
+import IEvaluation from "./IEvaluation";
+import IService from "./IService";
+import IOffer from "./IOffer";
+
+export type {
+  IAddress,
+  IUser,
+  IProvider,
+  ICategory,
+  IChat,
+  IEvaluation,
+  IService,
+  IOffer,
+};
