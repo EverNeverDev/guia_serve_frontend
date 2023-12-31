7c09938aa1857ab00e06797260859b24682edb92
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-30 23:08:49 -0300

    :sparkles: feat: created customNavbar

---
+++ src/app/common/components/CustomNavbar.tsx
@@ -0,0 +1,27 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+
+const CustomNavbar = () => {
+  return (
+    <Navbar className="bg-primary" maxWidth="xl">
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Guia serve logo</p>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button as={Link} color="secondary" href="#" variant="flat">
+            Fazer login
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+};
+
+export default CustomNavbar;
